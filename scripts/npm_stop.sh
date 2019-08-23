5e392581910fd93cca79a95c6870465d946704db
Firman Budi Safrizal <firmanbudis@gmail.com>
2019-08-23 16:30:05 +0700

    test1

--- scripts/npm_stop.sh
+++ scripts/npm_stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cd /home/ubuntu/filedrop-api-nodejs
-rm -rf /home/ubuntu/filedrop-api-nodejs/*
+#rm -rf /home/ubuntu/filedrop-api-nodejs/*
 #killall node
 pm2 stop index
 #rm -rf node_modules
